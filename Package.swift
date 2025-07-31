ac06da860a3f73945905fda7d98924c25ae74265
johnbean393 <johnbean393@gmail.com>
2025-07-31 14:19:15 +0800

    fix: Update dependencies

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "ExtractKit-macOS",
 	platforms: [
-		.macOS(.v13)
+		.macOS(.v15)
 	],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
